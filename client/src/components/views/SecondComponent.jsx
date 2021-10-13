9252de21079ea0d822e66094959d09ff0466d54f
yanggwangseong <rhkdtjd_12@naver.com>
2021-10-13 22:56:55 +0900

    이미지 저화질로 바꿈

--- client/src/components/views/SecondComponent.jsx
+++ client/src/components/views/SecondComponent.jsx
@@ -5,9 +5,7 @@ function SecondComponent() {
     return (
         <div>
            <Fade cascade>
-                <p>I enter first...</p>
-                <p>...then comes my turn...</p>
-                <p>...and finally you see me!</p>
+                <p>I enter tow...</p>
             </Fade>
         </div>
     )
